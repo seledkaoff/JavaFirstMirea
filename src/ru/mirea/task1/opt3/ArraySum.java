a874fdf8ecf3368b7a30131a036d56c489054f9b
simasomis <simasomis@mail.ru>
2024-09-06 10:25:36 +0300

    first and second task complete

--- src/ru/mirea/task1/opt3/ArraySum.java
+++ src/ru/mirea/task1/opt3/ArraySum.java
@@ -1,8 +1,18 @@
 package ru.mirea.task1.opt3;
+import java.util.Scanner;
 
 public class ArraySum {
     public static void main(String[] args) {
-        int[] digits = {1, 2, 3, 4, 5};
+        Scanner scan = new Scanner(System.in);
+        System.out.printf("Введите длину массива: ");
+        int len = scan.nextInt();
+        int[] digits = new int[len];
+        System.out.printf("\nВведите %d целых чисел: ", len);
+
+        for (int i = 0; i < len; i++){
+            digits[i] = scan.nextInt();
+        }
+
         int sum = 0;
 
         for (int i = 0; i < digits.length; i++) {
--- src/ru/mirea/task1/opt4/ShowArgs.java
+++ src/ru/mirea/task1/opt4/ShowArgs.java
@@ -1,4 +1,30 @@
 package ru.mirea.task1.opt4;
-
+import java.util.Scanner;
 public class ShowArgs {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.printf("Введите длину массива: ");
+        int len = scan.nextInt();
+        int[] digits = new int[len];
+        System.out.printf("\nВведите %d целых чисел: ", len);
+
+        for (int i = 0; i < len; i++){
+            digits[i] = scan.nextInt();
+        }
+
+        int sumFirst = 0;
+        int i = 0;
+        while (i < len){
+            sumFirst += digits[i];
+            i++;
+        }
+        int sumSecond = 0;
+        int j = 0;
+        do{
+            sumSecond += digits[j];
+            j++;
+        } while(j < len);
+
+        System.out.printf("sum1 = %d, sum2 = %d, равны = %b", sumFirst, sumSecond, sumFirst = sumSecond);
+    }
 }
